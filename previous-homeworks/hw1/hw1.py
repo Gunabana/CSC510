805eb0b360cbb6e302552dc55dd7f76d6f9ed377
brodybond <149192242+brodybond@users.noreply.github.com>
2024-09-03 17:39:17 +0000

    Moved work to HW1 directory in order to work on HW2

---
+++ previous-homeworks/hw1/hw1.py
@@ -0,0 +1,12 @@
+def execute(inp):
+    arr = []
+    if inp in ("Y", "y"):
+        arr[-1]	# error here
+    elif inp in ("N", "n"):
+        print("Okay, that is smart. Shutting down.")
+    else:
+        print("Invalid input. Shutting down.")
+
+if __name__ == "__main__":
+    print("Hey, I heard you want to download a virus!")
+    execute(input("Do you want a virus? (Y/n)\n>"))
