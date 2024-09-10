1337f986c9e288b781b3ed7ed6381b972df79562
brodybond <149192242+brodybond@users.noreply.github.com>
2024-09-10 13:20:56 -0400

    Bbond/badges (#2)
    * implement badges
    
    * changed perms for YML file to write
    
    * Update python-app.yml
    
    * push for workflow
    
    * removed third party dependency
    
    * tweaks to YML
    
    * workflow updates
    
    * workflow updates
    
    * Update Pylint and Radon badges
    
    * tweak YML; removed wording of ... URL
    
    * YML tweaks for github commit
    
    * Update Pylint and Radon badges
    
    * tweaks to YML
    
    * Update Pylint and Radon badges
    
    * Update Pylint and Radon badges
    
    * YML tweaks
    
    * Update Pylint and Radon badges
    
    * Update README.md
    
    * Update Pylint and Radon badges
    
    * YML tweaks
    
    * YML tweaks; cleaner file structure
    
    * Update Pylint and Radon badges
    
    * Removed proper formatting to test workflow
    
    * Update Pylint and Radon badges
    
    * Fixed to comply with lint
    
    * Update Pylint and Radon badges
    
    * Tweaks to gitignore
    
    * Manually reverting back to old makefile
    
    * Update python-app.yml
    
    * Tweak to YML
    
    * tweaks to YML
    
    * tweaks to YML
    
    * Manual revert to prior version of YML
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- test_hw2_debugging.py
+++ test_hw2_debugging.py
@@ -1,14 +1,18 @@
 """Import the pytest suite"""
 from hw2_debugging import merge_sort
 
+
 def test_single_value():
     """Tests that a single element is returned"""
     assert merge_sort([1]) == [1]
+
 
 def test_two_values():
     """Tests that two elements are sorted"""
     assert merge_sort([2, 1]) == [1, 2]
 
+
 def test_long_array():
     """Test a long array"""
-    assert merge_sort([21, 48, 21, 2, 0, 4, -3, 100]) == [-3, 0, 2, 4, 21, 21, 48, 100]
+    assert merge_sort([21, 48, 21, 2, 0, 4, -3, 100]
+                      ) == [-3, 0, 2, 4, 21, 21, 48, 100]
